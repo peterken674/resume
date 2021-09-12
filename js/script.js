e6bd45691b2022c18ca83586b176f257314ad6d4
Peter Kennedy <peterken.ngugi@gmail.com>
2021-09-12 11:29:10 +0300

    Add page animations.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,78 @@
-$(window).ready(function(){
-    let width = window.innerWidth;
-    if (width <= 991) {
-        $('.nav-link').click(function(){
-            $('.navbar-toggler').trigger('click');
-        });
+$(window).ready(function () {
+  let width = window.innerWidth;
+  if (width <= 991) {
+    $(".nav-link").click(function () {
+      $(".navbar-toggler").trigger("click");
+    });
+  }
+
+  $("img").ready(function () {
+    $(".loading-page").fadeOut("slow");
+  });
+
+  var scroll =
+    window.requestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+
+  var elementsToShow = document.querySelectorAll(".resume-section-content");
+
+  function loop() {
+    elementsToShow.forEach(function (element) {
+      if (isElementInViewport(element)) {
+        element.classList.add("is-visible");
+      } else {
+        element.classList.remove("is-visible");
+      }
+    });
+
+    scroll(loop);
+  }
+
+  loop();
+
+  // Helper function from: http://stackoverflow.com/a/7557433/274826
+  function isElementInViewport(el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0 && rect.bottom >= 0) ||
+      (rect.bottom >=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <=
+          (window.innerHeight || document.documentElement.clientHeight)) ||
+      (rect.top >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
+
+  var previousPosition =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  window.onscroll = function () {
+    var currentPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (previousPosition > currentPosition) {
+      $(".resume-section-content").css("transform", "translateY(-100px)");
+
+      $(".resume-section-content.is-visible").css(
+        "transform",
+        "translateY(0px)"
+      );
+    } else {
+        $(".resume-section-content").css("transform", "translateY(100px)");
+
+        $(".resume-section-content.is-visible").css(
+          "transform",
+          "translateY(0px)"
+        );
     }
 
-    $("img").ready(function(){
-        $(".loading-page").fadeOut("slow");
-    });
+    previousPosition = currentPosition;
+  };
 });
