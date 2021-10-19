928b6b30fd820aceddafb6c5c64d3e1724a54246
Peter Kennedy <peterken.ngugi@gmail.com>
2021-10-19 17:28:00 +0300

    Hide menu on click.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(window).ready(function () {
   let width = window.innerWidth;
-  if (width <= 991) {
+  
     $(".nav-link").click(function () {
       $(".navbar-toggler").trigger("click");
     });
-  }
+
 
   $("img").ready(function () {
     $(".loading-page").fadeOut("slow");
